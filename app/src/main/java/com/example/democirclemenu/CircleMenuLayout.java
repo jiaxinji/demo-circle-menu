7696bda57d6beb8fe5761dfa3e36fc65fb60f75c
jiaxinji <jiaxinji417@163.com>
2022-07-20 15:28:56 +0800

    layout文件

---
+++ app/src/main/java/com/example/democirclemenu/CircleMenuLayout.java
@@ -0,0 +1,15 @@
+package com.example.democirclemenu;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+public class CircleMenuLayout extends ViewGroup {
+    public CircleMenuLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+    }
+}
--- app/src/main/java/com/example/democirclemenu/MainActivity.java
+++ app/src/main/java/com/example/democirclemenu/MainActivity.java
@@ -6,6 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CircleMenuLayout mCircleMenuLayout;
+
+    private String[] mItemTexts = new String[] {"1号", "2号", "3号", "4号", "5号", "6号"};
+
+    private int[] mItemImgs = new int[] {R.drawable.home_mbank_1_clicked,
+            R.drawable.home_mbank_2_clicked, R.drawable.home_mbank_3_clicked,
+            R.drawable.home_mbank_4_clicked, R.drawable.home_mbank_5_clicked,
+            R.drawable.home_mbank_6_clicked};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
